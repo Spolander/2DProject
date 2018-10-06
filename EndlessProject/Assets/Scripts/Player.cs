fd0038a62d0f79c17ed4f5d9209804fd796cbaac
Spolander <38954068+Spolander@users.noreply.github.com>
2018-10-06 14:03:49 +0300

    unity project files added

---
+++ EndlessProject/Assets/Scripts/Player.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour {
+
+    float gravity = 0;
+    [SerializeField]
+    float movementSpeed = 1;
+    Animator anim;
+
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float jumpForce = 6;
+
+    private float jumpTimer = 0;
+
+    [SerializeField]
+    private float jumpWindow = 0.2f;
+
+    private float lastJumpTime = 0;
+
+    bool jumping = false;
+    public bool Jumping { set { jumping = value; } }
+
+    float flippingSpeed = 720f;
+
+    bool canJump = false;
+
+    [SerializeField]
+    private Vector2[] raycastPoints;
+
+    [SerializeField]
+    private float wallCheckDistance = 0.2f;
+
+    public LayerMask collisionLayers;
+
+    [SerializeField]
+    public GameObject bulletPrefab;
+
+    
+    private Vector2 defaultBulletSpawnPoint = new Vector2(0.28f,0.29f);
+
+    [SerializeField]
+    private float rateOfFire = 0.2f;
+
+    private float lastFireTime = 0;
+
+    [SerializeField]
+    private float projectileSpeed = 15;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(horizontal) > 0.2f ? 1 : 0);
+
+        if (Input.GetButton("Fire1"))
+        {
+            if (Time.time > lastFireTime + rateOfFire)
+            {
+                lastFireTime = Time.time;
+                GameObject g = (GameObject)Instantiate(bulletPrefab, transform.TransformPoint(defaultBulletSpawnPoint), Quaternion.identity);
+                g.GetComponent<projectile>().Initialize(transform.right*Mathf.Sign(transform.localScale.x), projectileSpeed);
+            }
+        }
+
+        if (horizontal > 0 && transform.localScale.x < 0)
+            Flip();
+        else if (horizontal < 0 && transform.localScale.x > 0)
+            Flip();
+
+        anim.SetLayerWeight(1, Input.GetButton("Fire1") ? 1 : 0);
+
+        if (Input.GetButton("Jump") && canJump)
+        {
+            Jump();
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            canJump = false;
+            jumpTimer = 0;
+        }
+
+      
+    }
+
+  
+    private void FixedUpdate()
+    {
+        float horizontal = 0;
+        if (canMoveHorizontally())
+        {
+         horizontal = Input.GetAxisRaw("Horizontal");
+        }
+       
+
+        Vector2 velocity = new Vector2(horizontal * movementSpeed, rb.velocity.y);
+
+        rb.velocity = velocity;
+
+        
+    }
+
+    private void Jump()
+    {
+        jumpTimer += Time.deltaTime;
+
+        if (jumpTimer >= jumpWindow)
+        {
+            canJump = false;
+            return;
+        }
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        anim.SetBool("Grounded", false);
+        anim.Play("flip");
+        lastJumpTime = Time.time;
+        jumping = true;
+
+       
+    }
+
+    void Flip()
+    {
+        float scale = transform.localScale.x > 0 ? -1 : 1;
+        transform.localScale = new Vector3(scale, 1, 1);
+    }
+
+    bool canMoveHorizontally()
+    {
+        for (int i = 0; i < raycastPoints.Length; i++)
+        {
+            RaycastHit2D h = Physics2D.Raycast(transform.TransformPoint(raycastPoints[i]), Vector2.right * Mathf.Sign(transform.localScale.x), wallCheckDistance, collisionLayers);
+            if (h.collider)
+                return false;
+        }
+        return true;
+    }
+    private void OnDrawGizmos()
+    {
+        if (raycastPoints.Length > 0)
+        {
+            for (int i = 0; i < raycastPoints.Length; i++)
+                Gizmos.DrawRay(transform.TransformPoint(raycastPoints[i]), Vector2.right * Mathf.Sign(transform.localScale.x) * wallCheckDistance);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Time.time > lastJumpTime + 0.2f)
+        {
+            foreach (ContactPoint2D c in collision.contacts)
+                if (c.normal.y > 0.7f)
+                {
+                    anim.SetBool("Grounded", true);
+                    canJump = jumping ? false:true ;
+                    jumping = false;
+                }
+               
+        }
+    }
+}
---
+++ EndlessProject/Assets/Scripts/projectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour {
+
+    Vector3 direction;
+    float speed;
+
+    
+    private float hitBoxSize = 0.02f;
+
+    [SerializeField]
+    private LayerMask collisionLayers;
+
+    public void Initialize(Vector2 direction, float speed)
+    {
+        this.direction = direction.normalized;
+        
+        this.speed = speed;
+
+        if(direction.y == 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(direction.x), 1, 1);
+        }
+        else
+             transform.localEulerAngles = new Vector3(0, 0, Mathf.Atan(direction.y / direction.x) * Mathf.Rad2Deg);
+
+    }
+
+    private void Update()
+    {
+        transform.position += direction * Time.deltaTime * speed;
+        HitDetection();
+    }
+
+    void HitDetection()
+    {
+        Collider2D[] cols = new Collider2D[1];
+        Physics2D.OverlapBoxNonAlloc(transform.position, Vector2.one * hitBoxSize, 0, cols,collisionLayers);
+
+        if (cols[0])
+            Destroy(gameObject);
+    }
+
+   
+}
